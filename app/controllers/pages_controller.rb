9ec9543d71488424b375c90b3d9b70265324e09a
CharlieKB <charlieforce1999@gmail.com>
2017-05-27 00:49:07 -0700

    An app similar to buffer
    A buffer app similar to the app buffer

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,13 @@
+class PagesController < ApplicationController
+	before_action :authenticate_user!, only: [:dashboard]
+  def home
+  	if current_user
+  		redirect_to dashboard_path
+  	end
+  end
+
+  def dashboard
+  	@scheduled = current_user.posts.where(state: "scheduled").paginate(page: params[:scheduled_page], per_page: 4).order('scheduled_at ASC')
+  	@history = current_user.posts.where.not(state: "scheduled").paginate(page: params[:history_page], per_page: 4).order('scheduled_at DESC')
+  end
+end
---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,38 @@
+class PostsController < ApplicationController
+	before_action :set_post, only: [:cancel]
+
+	def new
+		if current_user.connections.any?
+			@post = Post.new
+		else
+			redirect_to dashboard_path, notice: "Please connect to a social network first"
+		end
+	end
+
+	def create
+		@post = current_user.posts.new(post_params)
+
+		respond_to do |format|
+			if @post.save
+				format.html {redirect_to dashboard_path, notice: "Post was successfully created."}
+			else
+				format.html { render :new }
+			end
+		end
+	end
+
+	def cancel
+		@post.update_attributes(state: "canceled")
+		redirect_to dashboard_path, notice: "Post was successfully canceled"
+	end
+
+	private
+
+	def set_post
+		@post = Post.find(params[:id])
+	end
+
+	def post_params
+		params.require(:post).permit(:content, :scheduled_at, :state, :user_id, :facebook, :twitter)
+	end
+end
