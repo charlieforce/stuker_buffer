91311919cf9db38d082a1e57c04defa22bbf8f8e
Tran Van Thanh <tranthanhepu@gmail.com>
2017-07-07 13:30:24 +0700

    fix

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -6,14 +6,16 @@ $(document).ready(function(){
           $('#post_content').keyup(function(){            
             var text_length = $('#post_content').val().length;
             var text_remaining = text_max - text_length;
-            $('#maxCharacters').html(text_remaining + ' characters remaining');
+            if(text_remaining > 0){
+              $('#maxCharacters').html(text_remaining + ' characters remaining');
+            }
 
             if(text_remaining <= 10) {
               $('#maxCharacters').css('color', 'red');
             }
 
-            if(text_remaining == 0) {              
-              $('#post_content').attr('readonly','readonly');               
+            if(text_remaining <= 0) {
+              $('#post_content').attr('disable','disable');
             }
           });                  
       } else {
