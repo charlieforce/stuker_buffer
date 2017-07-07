4943f1250caeaa03174e0a34db0468df2b9478ae
Tran Van Thanh <tranthanhepu@gmail.com>
2017-07-07 11:30:16 +0700

    Update check length with only twitter

---
+++ app/assets/javascripts/posts.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){  
+    $("#post_twitter").on("click", function(){
+      if(this.checked) {
+          var  text_max = 140;          
+          $('#maxCharacters').html(text_max + ' characters remaining');
+          $('#post_content').keyup(function(){            
+            var text_length = $('#post_content').val().length;
+            var text_remaining = text_max - text_length;
+            $('#maxCharacters').html(text_remaining + ' characters remaining');
+
+            if(text_remaining <= 10) {
+              $('#maxCharacters').css('color', 'red');
+            }
+
+            if(text_remaining == 0) {              
+              $('#post_content').attr('readonly','readonly');               
+            }
+          });                  
+      } else {
+          alert("Checkbox is unchecked.");
+      }
+    }); 
+});
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ActiveRecord::Base
 	belongs_to :user
 	validates_presence_of :content
 	validates_presence_of :scheduled_at
-	validates_length_of :content, maximum: 140, message: "less than 140 please"
+	validates_length_of :content, maximum: 140, message: "less than 140 please", :if => "twitter == t"
 	validates_datetime :scheduled_at, :on => :create, :on_or_after => Time.zone.now
 	after_create :schedule
 
